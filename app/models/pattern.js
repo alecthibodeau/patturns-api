08dc80744c7b4dc0ec01c890b31d7629e7b7d64b
alecthibodeau <alecthibodeau@gmail.com>
2018-08-28 08:29:25 -0400

    Add niceDate field.

--- app/models/pattern.js
+++ app/models/pattern.js
@@ -10,6 +10,10 @@ const patternSchema = new mongoose.Schema({
     default: Array(100),
     required: true
   },
+  niceDate: {
+    type: String,
+    required: true
+  },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
